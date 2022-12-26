d9fdd3208cd54aee7d4f7c555bc512648747f630
Md Anik Hossain <anikh499@gmail.com>
2022-12-26 19:00:44 +0600

    module 2 start(data-structure)

--- Basic Data Structures and Problem Solving/Module-1/module1.cpp
+++ Basic Data Structures and Problem Solving/Module-1/module1.cpp
@@ -1,9 +1,58 @@
-#include <iostream>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout<<"hello world"<<endl;
+//    return 0;
+//}
 
+
+//
+//#include<iostream>
+//#include<fstream>
+
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    cout<<"hello world"<<endl;
+    ofstream of;
+    of.open("1.txt");
+
+    ofstream of2;
+    of2.open("2.txt");
+
+    ifstream anik;
+    anik.open("3.txt");
+    int x;
+    double y,z;
+
+    anik >> x>> y>>z;
+
+    cout <<x <<" "<< y<<" " <<z ;
+    anik.close();
+
+//    of<<"Hello world";
+//    of2<<"Hello world 2222";
+//    of.close();
+//    of2.close();
+
     return 0;
 }
+
+//    freopen("output.txt","w",stdout);
+//
+//    ifstream of2
+//    of2.open("2.txt");
+
+
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//
+//int main()
+//{
+//    double num = 3.14162835;
+//    cout<<setprecision(4)<<fixed<<num;
+//}
--- Introduction to Programming Language/Module-28/module28.c
+++ Introduction to Programming Language/Module-28/module28.c
@@ -1,28 +1,48 @@
 #include<stdio.h>
 
-int func(int a,int b,int* large,int* small)
+int  main()
 {
+    int  a=4, *pi;
+    double b=9.55, *pd;
 
-    *large=a>b?a:b;
-    *small=a<b?a:b;
-//    if(a>b)
-//    {
-//        *large=a;
-//        *small=b;
-//    }
-//    else
-//    {
-//        *large=b;
-//        *small=a;
-//    }
-}
-int main()
-{
-    int a=10,b=25;
-    int  large,small;
+    pi=&a;
+    pd=&b;
 
-    func(a,b,&large,&small);
-    printf("%d %d",large,small);
-
+    printf("%d %lf \n",a,b);
+    printf("%p %p",pi,pd);
     return 0;
 }
+
+
+
+
+
+
+//#include<stdio.h>
+//
+//int func(int a,int b,int* large,int* small)
+//{
+//
+//    *large=a>b?a:b;
+//    *small=a<b?a:b;
+////    if(a>b)
+////    {
+////        *large=a;
+////        *small=b;
+////    }
+////    else
+////    {
+////        *large=b;
+////        *small=a;
+////    }
+//}
+//int main()
+//{
+//    int a=10,b=25;
+//    int  large,small;
+//
+//    func(a,b,&large,&small);
+//    printf("%d %d",large,small);
+//
+//    return 0;
+//}
