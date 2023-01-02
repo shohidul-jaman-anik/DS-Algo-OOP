10fcdee3b248f93f7ebd5cf8002ba99991607940
Md Anik Hossain <anikh499@gmail.com>
2023-01-03 01:50:57 +0600

    module 3 start

--- Basic Data Structures and Problem Solving/Module-2/module2.cpp
+++ Basic Data Structures and Problem Solving/Module-2/module2.cpp
@@ -1,22 +1,108 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
-string erase_first_last(string n)
-{
-    n.erase(n.begin());
-    n.pop_back();
-    return n;
-}
 int main()
 {
-    string s;
-    getline(cin ,s);
+    vector<int>a= {33,12,5,9,66,54,18,77,18};
 
-    string ans = erase_first_last(s);
-    cout<<ans;
+    sort(a.begin()+0, a.begin()+5+1);
+
+    for(int i=0; i<a.size(); i++)
+    {
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
+
+
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//int main()
+//{
+//    int a=4, b=10;
+//    int ans1=min(a,b);
+//    int ans2=max(a,b);
+//    cout<<ans1<< " "<<ans2;
+//    return 0;
+//}
+
+
+
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//int main()
+//{
+//    vector <int> a={1,2,3};
+//    a.resize(5);
+//    for(int i=0; i<a.size(); i++)
+//    {
+//        cout<<a[i];
+//    }
+//    return 0;
+//}
+
+
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//int main()
+//{
+//    vector <int> a;
+//    for(int i=0; i<10; i++)
+//    {
+//        cout<<"Before push"<<a.size()<<"\n";
+//        a.push_back(i);
+//        cout<<"After push"<<a.size()<<"\n";
+//    }
+//    a.insert(a.begin()+0,100);
+//    for(int i=0; i<a.size(); i++)
+//    {
+//        cout<<a[i];
+//    }
+//    return 0;
+//}
+
+
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//int main()
+//{
+//    vector <int> a;
+//
+//    a.push_back(1);
+//    a.push_back(2);
+//    a.push_back(3);
+//
+//    for(int i=0; i<a.size(); i++)
+//    {
+//        cout<<a[i];
+//    }
+//    return 0;
+//}
+
+
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//
+//string erase_first_last(string n)
+//{
+//    n.erase(n.begin());
+//    n.pop_back();
+//    return n;
+//}
+//int main()
+//{
+//    string s;
+//    getline(cin ,s);
+//
+//    string ans = erase_first_last(s);
+//    cout<<ans;
+//    return 0;
+//}
 
 
 //#include<bits/stdc++.h>
---
+++ Basic Data Structures and Problem Solving/Module-4/module4.cpp
@@ -0,0 +1,38 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    int a, b, c, d, e, maxi;
+//    cin >> a >> b >> c >> d >> e;
+//    maxi = max({ a,b,c,d,e });
+//    cout << maxi << '\n';
+//    return 0;
+//}
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    vector<int> v(n);
+//    for (int i = 0;i < n;i++)
+//        cin >> v[i];
+//    int ans = 1;
+//    for (int i = 0;i < n;i++)
+//        ans *= v[i];
+//
+//    cout << ans << '\n';
+//    return 0;
+//}
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, maxi;
+    cin >> a >> b >> c >> d >> e;
+    maxi = max({ a,b,c,d,e });
+    cout << maxi << '\n';
+    return 0;
+}
