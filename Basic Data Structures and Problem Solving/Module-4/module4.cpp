d066a5b4e56edc9e2e4778ce98a8eb99fc634382
Md Anik Hossain <anikh499@gmail.com>
2023-01-08 23:28:03 +0600

    module-5 start

--- Basic Data Structures and Problem Solving/Module-4/module4.cpp
+++ Basic Data Structures and Problem Solving/Module-4/module4.cpp
@@ -2,19 +2,113 @@
 using namespace std;
 int main()
 {
-    int a, b, c;
-    cin >> a >> b >> c ;
-    int maxi = max({ a,b,c});
-    int mini =min({a,b,c});
-    int sum=a+b+c;
-    int mul=a*b*c;
-    cout << maxi << '\n';
-    cout << mini << '\n';
-    cout << sum << '\n';
-    cout << mul << '\n';
+    int n;
+    cin >> n;
+    vector <int>a(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        string ans="No\n";
+        for(int j=0; j<n; j++)
+        {
+            if(i==j)
+                continue;
+            if(a[i]==a[j])
+            {
+                ans="Yes\n";
+            }
+        }
+        cout <<"i= "<<i<<" "<<ans;
+    }
 
     return 0;
 }
+
+
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    vector <int>a(n);
+//    for(int i=0; i<n; i++)
+//    {
+//        cin>>a[i];
+//    }
+//    int maxi = a[0];
+//    int mini =a[0];
+//    int sum=0;
+//
+//    for(int i=0; i<n; i++)
+//    {
+//        maxi = max(maxi,a[i]);
+//        mini =min(mini,a[i]);
+//        sum=sum+a[i];
+//    }
+//
+//    cout << maxi << '\n';
+//    cout << mini << '\n';
+//    cout << sum << '\n';
+//
+//    return 0;
+//}
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    vector<int> v(5, 2);
+//    for (int i = 0;i < 5 - 2;i++)
+//        v[i] = i;
+//    for (int i = 0;i < 5;i++)
+//        cout << v[i] << " ";
+//    return 0;
+//}
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    vector<int> v(5,0);
+//    v[1] = 5;
+//    cout << v.front() << '\n';
+//    return 0;
+//}
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    vector<int> v(5, 0);
+//    v.resize(10, 10);
+//    cout << v[7] << '\n';
+//    return 0;
+//}
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    int a, b, c;
+//    cin >> a >> b >> c ;
+//    int maxi = max({ a,b,c});
+//    int mini =min({a,b,c});
+//    int sum=a+b+c;
+//    int mul=a*b*c;
+//    cout << maxi << '\n';
+//    cout << mini << '\n';
+//    cout << sum << '\n';
+//    cout << mul << '\n';
+//
+//    return 0;
+//}
 
 
 
