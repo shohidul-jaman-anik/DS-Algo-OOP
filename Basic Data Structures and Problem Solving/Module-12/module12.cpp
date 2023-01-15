d53364a9579989ee61a4c4f5ccfc0b0e9c642d61
Md Anik Hossain <anikh499@gmail.com>
2023-01-15 22:51:12 +0600

    module-5 uncomplete and right now  going to module-6

---
+++ Basic Data Structures and Problem Solving/Module-12/module12.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v(1);
+    v[0] = 9;
+    v.resize(9);
+    cout << v.back() << '\n';
+    return 0;
+}
+
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main() {
+//    vector<int> v[1];
+//    v[0].push_back(3);
+//    v[0].push_back(3);
+//    vector<int> t = v[0];
+//    cout << t[0] << '\n';
+//    return 0;
+//}
--- Basic Data Structures and Problem Solving/Module-5/module5.cpp
+++ Basic Data Structures and Problem Solving/Module-5/module5.cpp
@@ -1,1 +1,71 @@
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    cin>>n;
+//    int sum=0;
+//    for(int i=0; i<=n; i++)
+//    {
+//        sum=sum+i;
+//    }
+//    cout<<sum;
+//    return 0;
+//}
+
+
+
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector <int>number(n);
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>number[i];
+    }
+    int low=0;
+    int high=n-1;
+    bool flag=0;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        cout<<"low ="<<low <<"high = "<<high <<"mid ="<<mid <<"\n";
+        if(number[mid]==k)
+        {
+            cout<<"Yes "<<mid<<"\n";
+            flag=1;
+            break;
+        }
+        if(number[mid]>k)
+        {
+            high=mid-1;
+        }
+        else
+        {
+            //a[mid]<k
+            low=mid+1;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"No \n";
+    }
+    cout<<"\n";
+
+    return 0;
+}
+
+
+
+
+
+
 
