567e868af8cea90df3a10c6169f3ca16cff3a354
Md Anik Hossain <anikh499@gmail.com>
2023-01-21 17:55:40 +0600

    c pogramming 32 module done

--- Introduction to Programming Language/Module-32/module32.c
+++ Introduction to Programming Language/Module-32/module32.c
@@ -1,15 +1,84 @@
 #include<stdio.h>
-int foo(int n)
+
+int solve(int i,int n)
 {
-    if(n==0)
+    if(i>n)
+    {
         return 0;
-    return foo(n-1)+n;
+    }
+    int s=solve(i+1,n);
+    return s+i;
 }
+
 int main()
 {
-    printf("%d\n",foo(6));
+    int n;
+    scanf("%d",&n);
+    int ans=solve(1,n);
+    printf("%d",ans);
+
     return 0;
 }
+
+//#include<stdio.h>
+//
+//void solve(int n)
+//{
+//    if(n<1)
+//    {
+//        return;
+//    }
+//    printf("%d",n);
+//    solve(n-1);
+//}
+//
+//int main()
+//{
+//    int n;
+//    scanf("%d",&n);
+//    solve(n);
+//
+//    return 0;
+//}
+
+
+
+//#include<stdio.h>
+//
+//void solve(int i,int n)
+//{
+//    if(i>n)
+//    {
+//        return;
+//    }
+//    solve(i+1,n);
+//    printf("%d",i);
+//
+//}
+//
+//int main()
+//{
+//    int n;
+//    scanf("%d",&n);
+//    solve(1,n);
+//
+//    return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int foo(int n)
+//{
+//    if(n==0)
+//        return 0;
+//    return foo(n-1)+n;
+//}
+//int main()
+//{
+//    printf("%d\n",foo(6));
+//    return 0;
+//}
 
 //#include<stdio.h>
 //int foo(int n)
