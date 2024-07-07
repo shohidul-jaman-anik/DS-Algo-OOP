1b2158c11f185911ce17112739b78be024ccf9c1
shohidul-jaman-anik <anikh499@gmail.com>
2024-07-07 16:41:39 +0600

    revise done

---
+++ OOP and Python Programming/OOP Module 1/practise.py
@@ -0,0 +1,50 @@
+# number = int(input("Enter a number: "))
+
+# if number > 1000:
+#     print("You will get honey")
+# elif number>=500 & number<=800:
+#     print("you will get mobile")
+# else:
+#     print("you will get nothing")
+
+# =====================
+
+# number = 1
+
+# while number <= 10:
+#     number = number+1
+#     if number % 2 != 0:
+#         continue
+#     print(number)
+
+# =====================
+
+
+# text = "Bangladesh is the best country"
+
+# for i in range(0, len(text), 2):
+#     print(text[i])
+
+
+# =====================
+
+# def oddNumber(numbers):
+#     ans = []
+#     for number in numbers:
+
+#         if number % 2 != 0:
+#             print(number)
+#             ans.append(number)
+
+#     return ans
+
+
+# oddNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+# =====================
+
+alph = ("a", "b", "c", "d")
+ralph = reversed(alph)
+for x in ralph:
+  print(x)
